f3c28b1c44a7b27efdbb05594f40f815400735f7
Charles Muchene <charlesmuchene@gmail.com>
2017-07-24 17:55:08 +0300

    Setting up

---
+++ app/src/main/java/com/senseidevs/kotlinintro/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.senseidevs.kotlinintro
+
+import android.os.Bundle
+import android.support.design.widget.FloatingActionButton
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+        val fab = findViewById(R.id.fab) as FloatingActionButton
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show()
+        }
+    }
+}
