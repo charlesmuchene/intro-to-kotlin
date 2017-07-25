4fc1f6d1d443a18f25dea1f0321b63c81373a168
Charles Muchene <charlesmuchene@gmail.com>
2017-07-25 08:50:09 +0300

    Add some more android components

---
+++ app/src/main/java/com/senseidevs/kotlinintro/MyIntentService.java
@@ -0,0 +1,85 @@
+package com.senseidevs.kotlinintro;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.content.Context;
+
+/**
+ * An {@link IntentService} subclass for handling asynchronous task requests in
+ * a service on a separate handler thread.
+ * <p>
+ * helper methods.
+ */
+public class MyIntentService extends IntentService {
+
+    private static final String ACTION_FOO = "com.senseidevs.kotlinintro.action.FOO";
+    private static final String ACTION_BAZ = "com.senseidevs.kotlinintro.action.BAZ";
+
+
+    private static final String EXTRA_PARAM1 = "com.senseidevs.kotlinintro.extra.PARAM1";
+    private static final String EXTRA_PARAM2 = "com.senseidevs.kotlinintro.extra.PARAM2";
+
+    public MyIntentService() {
+        super("MyIntentService");
+    }
+
+    /**
+     * Starts this service to perform action Foo with the given parameters. If
+     * the service is already performing a task this action will be queued.
+     *
+     * @see IntentService
+     */
+    public static void startActionFoo(Context context, String param1, String param2) {
+        Intent intent = new Intent(context, MyIntentService.class);
+        intent.setAction(ACTION_FOO);
+        intent.putExtra(EXTRA_PARAM1, param1);
+        intent.putExtra(EXTRA_PARAM2, param2);
+        context.startService(intent);
+    }
+
+    /**
+     * Starts this service to perform action Baz with the given parameters. If
+     * the service is already performing a task this action will be queued.
+     *
+     * @see IntentService
+     */
+    public static void startActionBaz(Context context, String param1, String param2) {
+        Intent intent = new Intent(context, MyIntentService.class);
+        intent.setAction(ACTION_BAZ);
+        intent.putExtra(EXTRA_PARAM1, param1);
+        intent.putExtra(EXTRA_PARAM2, param2);
+        context.startService(intent);
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        if (intent != null) {
+            final String action = intent.getAction();
+            if (ACTION_FOO.equals(action)) {
+                final String param1 = intent.getStringExtra(EXTRA_PARAM1);
+                final String param2 = intent.getStringExtra(EXTRA_PARAM2);
+                handleActionFoo(param1, param2);
+            } else if (ACTION_BAZ.equals(action)) {
+                final String param1 = intent.getStringExtra(EXTRA_PARAM1);
+                final String param2 = intent.getStringExtra(EXTRA_PARAM2);
+                handleActionBaz(param1, param2);
+            }
+        }
+    }
+
+    /**
+     * Handle action Foo in the provided background thread with the provided
+     * parameters.
+     */
+    private void handleActionFoo(String param1, String param2) {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    /**
+     * Handle action Baz in the provided background thread with the provided
+     * parameters.
+     */
+    private void handleActionBaz(String param1, String param2) {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
